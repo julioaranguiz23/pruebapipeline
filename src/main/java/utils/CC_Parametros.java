3378357504ae2c4c0c41a880065e463027edf6be
julioaranguiz23 <julio.aranguiz@testgroup.cl>
2025-05-20 16:23:17 -0400

    Add files via upload

--- src/main/java/utils/CC_Parametros.java
+++ src/main/java/utils/CC_Parametros.java
@@ -6,7 +6,7 @@ public interface CC_Parametros {
     String nombreAutomatizador = "[Flujo-Automatizado]";
     String nombreProyecto = "[*Proyecto*]";
     
-    String gloDir = "c://Estructura_Base//CC_Logs_2p//";
+    String gloDir = "CC_Logs_2p//";
     
     //Archivo CSV con los datos de entrada
     int gloColumnas = 5;
